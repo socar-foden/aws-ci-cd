25467833fa8fbce60ddf466fe473f4c6d61ecba2
Brandon <brandon@soomgo.com>
2021-11-30 06:58:48 +0900

    경로 수정

--- server.js
+++ server.js
@@ -2,10 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-app.use(express.static(path.resolve(__dirname, '../build')));
+app.use(express.static(path.resolve(__dirname, './build')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.resolve(__dirname, '../build', 'index.html'));
+  res.sendFile(path.resolve(__dirname, './build', 'index.html'));
 });
 
 app.listen(8080, () => {
